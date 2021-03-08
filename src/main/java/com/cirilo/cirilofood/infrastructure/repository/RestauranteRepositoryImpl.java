36dac03c2dca744ebc3a9f4a39bc3c087f62b03c
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-03-08 16:17:27 -0300

    criteria api

--- src/main/java/com/cirilo/cirilofood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/cirilo/cirilofood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import com.cirilo.cirilofood.domain.model.Restaurante;
 
@@ -20,7 +22,8 @@ public class RestauranteRepositoryImpl {
     private EntityManager manager;
 
     // using JPQL
-    // public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+    // public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial,
+    // BigDecimal taxaFreteFinal) {
 
     // var jpql = new StringBuilder();
     // jpql.append("from Restaurante where 0 = 0 ");
@@ -42,7 +45,8 @@ public class RestauranteRepositoryImpl {
     // parametros.put("taxaFinal", taxaFreteFinal);
     // }
 
-    // TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+    // TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(),
+    // Restaurante.class);
     // parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
 
     // return query.getResultList();
@@ -54,7 +58,15 @@ public class RestauranteRepositoryImpl {
         CriteriaBuilder builder = manager.getCriteriaBuilder();
 
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-        criteria.from(Restaurante.class); // from Restaurante
+        Root<Restaurante> root = criteria.from(Restaurante.class);
+
+        Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+
+        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+
+        Predicate taxaFinalPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate); // from Restaurante
 
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
         return query.getResultList();
