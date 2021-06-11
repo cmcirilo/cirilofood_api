14de46237135a28d6cd25b59d617ad9c9b723d97
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-06-11 16:33:21 -0300

    using property message in constraints annotations (bean validation)

--- src/main/java/com/cirilo/cirilofood/api/controller/EstadoController.java
+++ src/main/java/com/cirilo/cirilofood/api/controller/EstadoController.java
@@ -1,12 +1,12 @@
 package com.cirilo.cirilofood.api.controller;
 
 import java.util.List;
-import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cirilo.cirilofood.domain.exception.EntidadeEmUsoException;
-import com.cirilo.cirilofood.domain.exception.EntidadeNaoEncontradaException;
 import com.cirilo.cirilofood.domain.model.Estado;
 import com.cirilo.cirilofood.domain.repository.EstadoRepository;
 import com.cirilo.cirilofood.domain.service.EstadoService;
@@ -27,40 +25,40 @@ import com.cirilo.cirilofood.domain.service.EstadoService;
 @RequestMapping("/estados")
 public class EstadoController {
 
-	@Autowired
-	private EstadoRepository estadoRepository;
-	
-	@Autowired
-	private EstadoService estadoService;
-	
-	@GetMapping
-	public List<Estado> listar() {
-		return estadoRepository.findAll();
-	}
-	
-	@GetMapping("/{estadoId}")
-	public Estado buscar(@PathVariable Long estadoId) {
-		return estadoService.buscar(estadoId);
-	}
-	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Estado adicionar(@RequestBody Estado estado) {
-		return estadoService.salvar(estado);
-	}
-	
-	@PutMapping("/{estadoId}")
-	public Estado atualizar(@PathVariable Long estadoId,
-			@RequestBody Estado estado) {
+    @Autowired
+    private EstadoRepository estadoRepository;
 
-		Estado estadoAtual = estadoService.buscar(estadoId);
-		BeanUtils.copyProperties(estado, estadoAtual, "id");
-		return estadoService.salvar(estadoAtual);
-	}
-	
-	@DeleteMapping("/{estadoId}")
-	public void remover(@PathVariable Long estadoId) {
-		estadoService.excluir(estadoId);
-	}
-	
+    @Autowired
+    private EstadoService estadoService;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return estadoRepository.findAll();
+    }
+
+    @GetMapping("/{estadoId}")
+    public Estado buscar(@PathVariable Long estadoId) {
+        return estadoService.buscar(estadoId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Estado adicionar(@RequestBody @Valid Estado estado) {
+        return estadoService.salvar(estado);
+    }
+
+    @PutMapping("/{estadoId}")
+    public Estado atualizar(@PathVariable Long estadoId,
+            @RequestBody @Valid Estado estado) {
+
+        Estado estadoAtual = estadoService.buscar(estadoId);
+        BeanUtils.copyProperties(estado, estadoAtual, "id");
+        return estadoService.salvar(estadoAtual);
+    }
+
+    @DeleteMapping("/{estadoId}")
+    public void remover(@PathVariable Long estadoId) {
+        estadoService.excluir(estadoId);
+    }
+
 }
