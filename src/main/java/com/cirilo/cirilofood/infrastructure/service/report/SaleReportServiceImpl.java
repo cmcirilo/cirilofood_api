12154b4bc9b1ff466b0d1a86d3629684ee4bed68
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-10-25 20:21:56 -0300

    generating daily sales pdf file

--- src/main/java/com/cirilo/cirilofood/infrastructure/service/report/SaleReportServiceImpl.java
+++ src/main/java/com/cirilo/cirilofood/infrastructure/service/report/SaleReportServiceImpl.java
@@ -1,14 +1,43 @@
 package com.cirilo.cirilofood.infrastructure.service.report;
 
-import com.cirilo.cirilofood.domain.filter.DailySaleFilter;
-import com.cirilo.cirilofood.domain.service.SaleReportService;
+import java.util.HashMap;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cirilo.cirilofood.domain.filter.DailySaleFilter;
+import com.cirilo.cirilofood.domain.service.SaleQueryService;
+import com.cirilo.cirilofood.domain.service.SaleReportService;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class SaleReportServiceImpl implements SaleReportService {
 
+    @Autowired
+    private SaleQueryService saleQueryService;
+
     @Override
     public byte[] buildDailySales(DailySaleFilter dailySaleFilter, String timeOffSet) {
-        return null;
+        try {
+
+            var inputStream = this.getClass()
+                    .getResourceAsStream("/reports/daily-sales.jasper");
+
+            var parameters = new HashMap<String, Object>();
+            parameters.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+            var dailySales = saleQueryService.findDailySales(dailySaleFilter, timeOffSet);
+            var dataSource = new JRBeanCollectionDataSource(dailySales);
+
+            var jasperPrint = JasperFillManager.fillReport(inputStream, parameters, dataSource);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (Exception ex) {
+            throw new ReportException("It's not possible generate report at this moment ", ex);
+        }
     }
 }
