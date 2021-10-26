523ec01ea8cfeb38769d25f67942b6f50832b4e1
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-10-26 10:01:03 -0300

    creating endpoint to upload product photo

---
+++ src/main/java/com/cirilo/cirilofood/api/controller/RestaurantProductPhotoController.java
@@ -0,0 +1,36 @@
+package com.cirilo.cirilofood.api.controller;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cirilo.cirilofood.api.model.input.ProductPhotoInput;
+
+@RestController
+@RequestMapping("/restaurants/{restaurantId}/products/{productId}/photo")
+public class RestaurantProductPhotoController {
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void updatePhoto(@PathVariable Long restaurantId, @PathVariable Long productId,
+            ProductPhotoInput productPhotoInput) {
+
+        var fileName = UUID.randomUUID()
+                + "_" + productPhotoInput.getFile().getOriginalFilename();
+
+        var filePhoto = Path.of("/home/cirilos/Documents/Projects", fileName);
+
+        try {
+            productPhotoInput.getFile().transferTo(filePhoto);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/main/java/com/cirilo/cirilofood/api/model/input/ProductPhotoInput.java
@@ -0,0 +1,16 @@
+package com.cirilo.cirilofood.api.model.input;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductPhotoInput {
+
+    private MultipartFile file;
+
+    private String description;
+
+}
