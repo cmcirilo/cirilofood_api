2a7fa15c355579365b7e7fe5dad5f95276bd2d75
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-11-04 11:53:18 -0300

    creating constraint validator to validate file type

--- src/main/java/com/cirilo/cirilofood/api/model/input/ProductPhotoInput.java
+++ src/main/java/com/cirilo/cirilofood/api/model/input/ProductPhotoInput.java
@@ -3,8 +3,11 @@ package com.cirilo.cirilofood.api.model.input;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import com.cirilo.cirilofood.core.validation.FileSize;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.cirilo.cirilofood.core.validation.FileContentType;
+import com.cirilo.cirilofood.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +18,7 @@ public class ProductPhotoInput {
 
     @NotNull
     @FileSize(max = "500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     private MultipartFile file;
 
     @NotBlank
