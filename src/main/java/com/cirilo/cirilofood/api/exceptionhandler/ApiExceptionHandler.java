9650f17f54c825c6a34ef8a7c5c132577b80a94d
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-11-06 21:14:41 -0300

    customizing handleHttpMediaTypeNotAcceptable

--- src/main/java/com/cirilo/cirilofood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/cirilo/cirilofood/api/exceptionhandler/ApiExceptionHandler.java
@@ -17,6 +17,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -106,6 +107,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 .build();
 
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status,
+            WebRequest request) {
+        return ResponseEntity.status(status).headers(headers).build();
     }
 
     @Override
