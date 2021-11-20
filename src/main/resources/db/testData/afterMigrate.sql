aa317f3d544358fcb3e1e922e43e265c4795b112
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-11-20 11:27:09 -0300

    adjusting freemarker template

--- src/main/resources/db/testData/afterMigrate.sql
+++ src/main/resources/db/testData/afterMigrate.sql
@@ -79,7 +79,7 @@ insert into restaurant_owner (restaurant_id, user_id) values (1, 5), (3, 5);
 delete from order_item;
 delete from `order`;
 
-insert into `order` (id, code, restaurant_id, user_client_id, form_payment_id, address_city_id, address_zipcode, address_street, address_number, address_complement, address_district, status, created_date, subtotal, shipping_fee, total_value) values (1, '61297945-cf33-4c3c-8aef-f0d50a08d3c2', 1, 1, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil','CREATED', utc_timestamp, 298.90, 10, 308.90);
+insert into `order` (id, code, restaurant_id, user_client_id, form_payment_id, address_city_id, address_zipcode, address_street, address_number, address_complement, address_district, status, created_date, subtotal, shipping_fee, total_value) values (1, '61297945-cf33-4c3c-8aef-f0d50a08d3c2', 1, 4, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil','CREATED', utc_timestamp, 298.90, 10, 308.90);
 insert into order_item (id, order_id, product_id, quantity, unit_price, total_price, observation) values (1, 1, 1, 1, 78.9, 78.9, null);
 insert into order_item (id, order_id, product_id, quantity, unit_price, total_price, observation) values (2, 1, 2, 2, 110, 220, 'Menos picante, por favor');
 
