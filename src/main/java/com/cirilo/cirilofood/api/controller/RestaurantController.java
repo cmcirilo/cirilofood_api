02f0598c5917cbb27793ba3909a89f280561d7b0
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-12-23 12:20:59 -0300

    implementing cross origin

--- src/main/java/com/cirilo/cirilofood/api/controller/RestaurantController.java
+++ src/main/java/com/cirilo/cirilofood/api/controller/RestaurantController.java
@@ -1,22 +1,5 @@
 package com.cirilo.cirilofood.api.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.cirilo.cirilofood.api.assembler.RestaurantInputDisassembler;
 import com.cirilo.cirilofood.api.assembler.RestaurantModelAssembler;
 import com.cirilo.cirilofood.api.model.RestaurantModel;
@@ -30,8 +13,21 @@ import com.cirilo.cirilofood.domain.model.Restaurant;
 import com.cirilo.cirilofood.domain.repository.RestaurantRepository;
 import com.cirilo.cirilofood.domain.service.RestaurantService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/restaurants")
 public class RestaurantController {
