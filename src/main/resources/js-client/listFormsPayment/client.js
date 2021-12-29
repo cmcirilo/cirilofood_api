2039a37a90935ce1e33db78f79bcb819be8b71c6
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2021-12-29 12:02:11 -0300

    implementing delete request using js client

--- src/main/resources/js-client/listFormsPayment/client.js
+++ src/main/resources/js-client/listFormsPayment/client.js
@@ -15,14 +15,76 @@ function loadTable(formsPayment) {
   $.each(formsPayment, function(i, formPayment) {
     var linha = $("<tr>");
 
+     var linkAcao = $("<a href='#'>")
+            .text("Remove")
+            .click(function(event) {
+              event.preventDefault();
+              remove(formPayment);
+            });
+
     linha.append(
       $("<td>").text(formPayment.id),
-      $("<td>").text(formPayment.description)
+      $("<td>").text(formPayment.description),
+      $("<td>").append(linkAcao)
     );
 
     linha.appendTo("#table");
   });
 }
 
+function save() {
+  var formPaymentJson = JSON.stringify({
+    "description": $("#field-description").val()
+  });
+
+  console.log(formPaymentJson);
+
+  $.ajax({
+    url: "http://localhost:8080/forms-payment",
+    type: "post",
+    data: formPaymentJson,
+    contentType: "application/json",
+
+    success: function(response) {
+      alert("Form Payment saved");
+      list();
+    },
+
+    error: function(error) {
+      if (error.status >= 400 && error.status <= 499) {
+        var problem = JSON.parse(error.responseText);
+        alert(problem.userMessage);
+      } else {
+        alert("Error saving form payment");
+      }
+    }
+  });
+}
+
+function remove(formPayment){
+    var url = "http://localhost:8080/forms-payment/" + formPayment.id;
+
+  $.ajax({
+    url: url,
+    type: "delete",
+
+    success: function(response) {
+      list();
+
+      alert("Form Payment removed!");
+    },
+
+    error: function(error) {
+      if (error.status >= 400 && error.status <= 499) {
+        var problem = JSON.parse(error.responseText);
+        alert(problem.userMessage);
+      } else {
+        alert("Error removing form payment");
+      }
+    }
+  });
+}
+
 
 $("#btn-list").click(list);
+$("#btn-save").click(save);
