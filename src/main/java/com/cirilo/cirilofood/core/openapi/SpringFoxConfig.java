bd6e75ccdaa3a95ae2ce5a89f1a3d7311e337812
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-03-10 15:32:24 -0300

    implementing configuration class of Swagger UI

--- src/main/java/com/cirilo/cirilofood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/cirilo/cirilofood/core/openapi/SpringFoxConfig.java
@@ -2,9 +2,10 @@ package com.cirilo.cirilofood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -18,11 +19,14 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.cirilo.cirilofood.api"))
+                .paths(PathSelectors.any())
+                // .paths(PathSelectors.ant("/restaurants/*"))
                 .build();
     }
 
-    @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
