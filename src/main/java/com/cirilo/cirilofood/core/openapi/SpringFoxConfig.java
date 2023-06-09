f611b0ac18e4c9f66b9951805db8389e3f2308c8
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-06-09 20:01:33 -0300

    implementing Documentation

--- src/main/java/com/cirilo/cirilofood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/cirilo/cirilofood/core/openapi/SpringFoxConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,7 +25,18 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.cirilo.cirilofood.api"))
                 .paths(PathSelectors.any())
-                // .paths(PathSelectors.ant("/restaurants/*"))
+                .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("Cities", "Manage the cities"));
+
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("CiriloFood API")
+                .description("API to clients and restaurants")
+                .version("1")
+                .contact(new Contact("CiriloFood", "https://www.cirilofood.com", "cirilofood@cirilofood.com"))
                 .build();
     }
 
