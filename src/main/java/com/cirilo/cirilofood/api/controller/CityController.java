47fad860660a9885598a4f684c87c9cf9f9a4b97
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-06-21 12:30:58 -0300

    implementing Documentation

--- src/main/java/com/cirilo/cirilofood/api/controller/CityController.java
+++ src/main/java/com/cirilo/cirilofood/api/controller/CityController.java
@@ -4,19 +4,9 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cirilo.cirilofood.api.assembler.CityInputDisassembler;
 import com.cirilo.cirilofood.api.assembler.CityModelAssembler;
@@ -27,6 +17,10 @@ import com.cirilo.cirilofood.domain.exception.EntityNotFoundException;
 import com.cirilo.cirilofood.domain.model.City;
 import com.cirilo.cirilofood.domain.repository.CityRepository;
 import com.cirilo.cirilofood.domain.service.CityService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Api(tags = "Cities")
 @RestController
@@ -54,7 +48,7 @@ public class CityController {
 
     @ApiOperation("Find City by Id")
     @GetMapping("/{cityId}")
-    public CityModel find(@PathVariable Long cityId) {
+    public CityModel find(@ApiParam(value = "City Id", example = "1") @PathVariable Long cityId) {
         City city = cityService.find(cityId);
         return cityModelAssembler.toModel(city);
     }
@@ -62,7 +56,7 @@ public class CityController {
     @ApiOperation("Create City")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CityModel create(@RequestBody @Valid CityInput cityInput) {
+    public CityModel create(@ApiParam(name = "body", value = "Representation of new city") @RequestBody @Valid CityInput cityInput) {
         try {
             City city = cityInputDisassembler.toDomainObject(cityInput);
             city = cityService.save(city);
@@ -75,8 +69,8 @@ public class CityController {
 
     @ApiOperation("Update City by Id")
     @PutMapping("/{cityId}")
-    public CityModel update(@PathVariable Long cityId,
-            @RequestBody @Valid CityInput cityInput) {
+    public CityModel update(@ApiParam(value = "City Id", example = "1") @PathVariable Long cityId,
+                            @ApiParam(name = "body", value = "Representation of updated city") @RequestBody @Valid CityInput cityInput) {
 
         try {
             City currentCity = cityService.find(cityId);
@@ -92,7 +86,7 @@ public class CityController {
     @ApiOperation("Delete City by Id")
     @DeleteMapping("/{cityId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long cityId) {
+    public void delete(@ApiParam(value = "City Id", example = "1")@PathVariable Long cityId) {
         cityService.delete(cityId);
     }
 
