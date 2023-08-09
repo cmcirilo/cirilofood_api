66ce596c06828b7dfb29bec5ff732e62bf96f4d6
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-08-09 18:15:15 -0300

    implementing Documentation

--- src/main/java/com/cirilo/cirilofood/api/controller/GroupController.java
+++ src/main/java/com/cirilo/cirilofood/api/controller/GroupController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ import com.cirilo.cirilofood.domain.repository.GroupRepository;
 import com.cirilo.cirilofood.domain.service.GroupService;
 
 @RestController
-@RequestMapping("/groups")
+@RequestMapping(path = "/groups", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GroupController implements GroupControllerOpenApi {
 
     @Autowired
