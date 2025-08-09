04c450289e922ce82b7eabbc397bdf7c926d1c21
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2025-08-09 19:53:52 -0300

    implementing Root Entry Point API

---
+++ src/main/java/com/cirilo/cirilofood/api/controller/RootEntryPointController.java
@@ -0,0 +1,40 @@
+package com.cirilo.cirilofood.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cirilo.cirilofood.api.CiriloLinks;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class RootEntryPointController {
+
+    @Autowired
+    private CiriloLinks ciriloLinks;
+
+    @GetMapping
+    public RootEntryPointModel root() {
+        var rootEntryPointModel = new RootEntryPointModel();
+
+        rootEntryPointModel.add(ciriloLinks.linkToCuisines("cuisines"));
+        rootEntryPointModel.add(ciriloLinks.linkToOrders("orders"));
+        rootEntryPointModel.add(ciriloLinks.linkToRestaurants("restaurants"));
+        rootEntryPointModel.add(ciriloLinks.linkToGroups("groups"));
+        rootEntryPointModel.add(ciriloLinks.linkToUsers("users"));
+        rootEntryPointModel.add(ciriloLinks.linkToPermissions("permissions"));
+        rootEntryPointModel.add(ciriloLinks.linkToFormsPayment("forms-payment"));
+        rootEntryPointModel.add(ciriloLinks.linkToStates("states"));
+        rootEntryPointModel.add(ciriloLinks.linkToCities("cities"));
+
+        return rootEntryPointModel;
+    }
+
+    private static class RootEntryPointModel extends RepresentationModel<RootEntryPointModel> {
+
+    }
+
+}
