9dd576fa166692757fe6b94285ca5bdb85ddfaa6
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2026-04-20 10:44:41 -0300

    Adjusting documentation

--- src/main/java/com/cirilo/cirilofood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/cirilo/cirilofood/core/springfox/SpringFoxConfig.java
@@ -53,16 +53,23 @@ import com.fasterxml.classmate.TypeResolver;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
 import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -126,6 +133,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 .alternateTypeRules(AlternateTypeRules.newRule(
                         typeResolver.resolve(CollectionModel.class, UserModel.class),
                         UsersModelOpenApi.class))
+                .securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()))
                 .apiInfo(apiInfoV1())
                 .tags(new Tag("Cities", "Manage the cities"),
                         new Tag("Groups", "Manage the groups"),
@@ -175,6 +184,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 .alternateTypeRules(AlternateTypeRules.newRule(
                         typeResolver.resolve(CollectionModel.class, CityModelV2.class),
                         CitiesModelV2OpenApi.class))
+                .securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()))
                 .apiInfo(apiInfoV2())
                 .tags(new Tag("Cities", "Manage the cities"),
                         new Tag("Cuisines", "Manage the cuisines"));
@@ -216,6 +227,35 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                         .responseModel(new ModelRef("Problem"))
                         .message("Internal Server Error")
                         .build());
+    }
+
+    private SecurityScheme securityScheme() {
+        return new OAuthBuilder()
+                .name("CiriloFood")
+                .grantTypes(grantTypes())
+                .scopes(scopes())
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        var securityReferente = SecurityReference.builder()
+                .reference("CiriloFood")
+                .scopes(scopes().toArray(new AuthorizationScope[0]))
+                .build();
+
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(securityReferente))
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<GrantType> grantTypes() {
+        return Arrays.asList(new ResourceOwnerPasswordCredentialsGrant("http://api.cirilofood.local:8080/oauth/token"));
+    }
+
+    private List<AuthorizationScope> scopes() {
+        return  Arrays.asList(new AuthorizationScope("READ","Read Access"),
+                new AuthorizationScope("WRITE","Write Access"));
     }
 
     private List<ResponseMessage> globalGetResponseMessages() {
