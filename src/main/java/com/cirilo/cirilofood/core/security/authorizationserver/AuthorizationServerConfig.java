a44458613dbd45f2520d4c31092863e87ec1f78d
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2026-09-02 18:07:39 -0300

    adjuste authorization codes

--- src/main/java/com/cirilo/cirilofood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/cirilo/cirilofood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -96,6 +97,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService)
                 .reuseRefreshTokens(false)
+                .authorizationCodeServices(new JdbcAuthorizationCodeServices(this.dataSource))
                 // .tokenStore(redisTokenStore())
                 .accessTokenConverter(jwtAccessTokenConverter())
                 .tokenEnhancer(enhancerChain)
